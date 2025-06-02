ab6e7dd5974d577a100a6f953214939beb8790bb
abdulaziz <A.Axmedov2@uzinfocom.uz>
2025-06-02 14:19:02 +0500

    del

--- config.py
+++
@@ -1,6 +0,0 @@
-TOKEN = "7250362954:AAGpUdnRI-L6BTpn8Oqiya_sj5XiplvdFVc"
-CHANNEL_USERNAME = "@tayyorlov31maktab"
-INSTAGRAM_URL = "https://www.instagram.com/sqlself"
-TELEGRAM_CHANNEL_URL = "https://t.me/tayyorlov31maktab"
-CONTACT_GROUP_ID = -1002584572304  # Group ID to forward contacts to
-ADMIN_IDS = [694558718, 5132031364]  # Admin Telegram user IDs
